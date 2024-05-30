8b9e2e07ca6de89afad9bc308669cace36fb9957
anjakely18 <anjaniaina03@gmail.com>
2024-05-30 12:36:40 +0200

    Read More

--- src/Tour.jsx
+++ src/Tour.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 const Tour = ({ id, name, info, image, price, removeTour }) => {
+  const [readMore, setReadMore] = useState(false);
+
   return (
     <article className="single-tour">
       <img src={image} alt={name} className="img" />
       <span className="tour-price">${price}</span>
       <div className="tour-info">
         <h5>{name}</h5>
-        <p>{info}</p>
+        {/*we don't want to return the entire text*/}
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+
+          <button
+            type="button"
+            className="info-btn"
+            onClick={() => {
+              setReadMore(!readMore);
+            }}
+          >
+            {readMore ? "show less" : "read more"}
+          </button>
+        </p>
         <button
           type="button"
           className="btn btn-block delete-btn"
